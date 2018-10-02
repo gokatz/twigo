a28da3aa5ad7ba73c0ed58b406b6d374393bf61a
gokatz <gokulkathirvel@live.com>
2018-10-02 23:19:09 +0530

    adds eslint

--- bot.js
+++ bot.js
@@ -1,35 +1,35 @@
-var twit = require('twit');
-var config = require('./config');
-var nodemailer = require('nodemailer');
+let twit = require('twit');
+let config = require('./config');
+let nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport. Using gmail can help us out in many things.
-var botEmail = '...'; //user name part of email, ie., john for <john@gmail.com> | this must ba a gmail or mailerArgs will be reconstructed
-var botPass = '...'; // password for <john@gmail.com>
+let botEmail = '...'; //user name part of email, ie., john for <john@gmail.com> | this must ba a gmail or mailerArgs will be reconstructed
+let botPass = '...'; // password for <john@gmail.com>
 
-var toMail = 'user_+name, user@mail.com'; // to mail address, eg: 'Abraham bran, abraham@yahoo.com'
-var mailerArgs = 'smtps://'+botEmail+'%40gmail.com:'+botPass+'@smtp.gmail.com';
-var transporter = nodemailer.createTransport(mailerArgs);
+let toMail = 'user_+name, user@mail.com'; // to mail address, eg: 'Abraham bran, abraham@yahoo.com'
+let mailerArgs = 'smtps://'+botEmail+'%40gmail.com:'+botPass+'@smtp.gmail.com';
+let transporter = nodemailer.createTransport(mailerArgs);
 
-var Twitter = twit(config);
+let Twitter = twit(config);
 
 // setup e-mail data with unicode symbols
-var mailOptions = {
-    from: '"Email Bot (Twigo)" <'+botEmail+'@gmail.com>', // sender address
-    to: toMail // list of receivers
+let mailOptions = {
+  from: '"Email Bot (Twigo)" <'+botEmail+'@gmail.com>', // sender address
+  to: toMail // list of receivers
 };
 
 //query params to query the tweets. Change the query string of your need
-var qparams = {
+let qparams = {
   q: '#emberjs, #emberJs, #emberJS, #EmberJs, #Emberjs, #EMBERJS',
   result_type: 'recent',
   lang: 'en'
 };
 
 // function to retweet tweets that matches my query string..
-var retweet = function() {
+let retweet = function() {
   Twitter.get('search/tweets', qparams, function(err, data) {
     if (!err) {
-      var retweetId = data.statuses[0].id_str;
+      let retweetId = data.statuses[0].id_str;
       Twitter.post('statuses/retweet/:id', {
         id: retweetId
       }, function(err, response) {
@@ -42,7 +42,7 @@ var retweet = function() {
           // Email Errors
           emailErrors(2, err);
         }
-      })
+      });
     } else {
       console.log('[Retweeting] OOPS! Something went wrong while querying tweets :(');
       // console.log(err);
@@ -50,21 +50,21 @@ var retweet = function() {
       emailErrors(1, err);
     }
   });
-}
+};
 
 //Favoriting a tweet
 
-var favoriteTweet = function(){
+let favoriteTweet = function(){
   // find the tweet
   Twitter.get('search/tweets', qparams, function(err,data){
 
     if (!err) {
       // find tweets
-      var tweet = data.statuses;
-      var randomTweet = selectRandom(tweet);   // pick a random tweet
+      let tweet = data.statuses;
+      let randomTweet = selectRandom(tweet);   // pick a random tweet
 
       if(typeof randomTweet != 'undefined'){
-        Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+        Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err){
           if(err){
             console.log('OOPS! Something went wrong while Favoriting a tweet :(');
             emailErrors(3, err);
@@ -81,7 +81,7 @@ var favoriteTweet = function(){
       emailErrors(1, err);
     }
   });
-}
+};
 
 retweet();
 setInterval(retweet, 3000000);
@@ -91,11 +91,11 @@ setInterval(favoriteTweet, 3600000);
 
 // function to retrun a random tweet from the tweet array
 function selectRandom (arr) {
-  var index = Math.floor(Math.random()*arr.length);
+  let index = Math.floor(Math.random()*arr.length);
   return arr[index];
-};
+}
 
-var emailErrors = function(type, err) {
+const emailErrors = function(type, err) {
   if (type == 1) {
     mailOptions.subject = 'Query: Failed';
   } else if (type == 2) {
@@ -104,14 +104,14 @@ var emailErrors = function(type, err) {
     mailOptions.subject = 'Query: Passed | Favorite: Failed';
   }
 
-  var errorString = 'error msg: '+err.message+' | error code: '+err.code;
+  let errorString = 'error msg: '+err.message+' | error code: '+err.code;
   mailOptions.text = errorString;
   mailOptions.html = '<code style="color: red;">'+errorString+'</code>';
 
   transporter.sendMail(mailOptions, function(error, info){
     if(error){
-        return console.log(error);
+      return console.log(error);
     }
     console.log('Error log is mailed to ' +toMail+ ' - Response : ' + info.response);
   });
-}
+};
